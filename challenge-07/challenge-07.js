f8ec04766033ef78efee042e30add1a127b5ab90
MalconMouraLima <malcon.moura.lima@gmail.com>
2019-02-14 16:55:57 -0400

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,26 +1,39 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+let arr = [ 7, 'Malcon', true, {cor:'azul'}, [ 1,2,3 ] ];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+function addItem(add){
+  arr.push(add);  
+  for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }  
+}
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+addItem([ 4,'5', false ]);
+  /*
+    7
+    Malcon
+    true
+    {cor: "azul"}
+    (3) [1, 2, 3]
+    (3) [4, "5", false]
+  */
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log('O segundo elemento do segundo array é ' + arr[5][1] + '.');
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
